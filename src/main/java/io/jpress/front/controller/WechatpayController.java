7bba99d65e9d0183b8a967e5ca81930a4958fa07
jianb.jiang <jianb.jiang@hnair.com>
2018-01-21 17:37:01 +0800

    微信支付

--- src/main/java/io/jpress/front/controller/WechatpayController.java
+++ src/main/java/io/jpress/front/controller/WechatpayController.java
@@ -2,8 +2,10 @@ package io.jpress.front.controller;
 
 import com.jfinal.aop.Before;
 import com.jfinal.kit.HttpKit;
+import com.jfinal.kit.PropKit;
 import com.jfinal.kit.StrKit;
 import com.jfinal.log.Log;
+import com.jfinal.plugin.activerecord.tx.Tx;
 import com.jfinal.weixin.sdk.api.ApiResult;
 import com.jfinal.weixin.sdk.api.PaymentApi;
 import com.jfinal.weixin.sdk.api.PaymentApi.TradeType;
@@ -42,85 +44,24 @@ public class WechatpayController extends BaseFrontController {
     String appid = OptionQuery.me().findValue("wechat_appid");
     String partner = OptionQuery.me().findValue("wechat_partner");
     String paternerKey = OptionQuery.me().findValue("wechat_paternerKey");
-    String notifyUrl = "http://m.yuweiguoye.com/wechatpay/pay";
+    String notifyUrl = PropKit.get("wechat_notify_url");
   
     //待支付-微信支付
     @Before(UCodeInterceptor.class)
     public void prepay() {
-        // openId，采用 网页授权获取 access_token API：SnsAccessTokenApi获取
-        String userJson = this.getSessionAttr(Consts.SESSION_WECHAT_USER);
-        String openId = new ApiResult(userJson).getStr("openid");
-
-        
-        final BigInteger id=getParaToBigInteger("id");
-
-        if (id==null) {
-            renderAjaxResultForError("订单id不能为空");
-            return;
+        try {
+            
+            String jsonStr = "{\"timeStamp\":\"1516526141\",\"package\":\"prepay_id=wx2018012117150606ac003fb20827751109\",\"paySign\":\"FCFF07F3A6E55C3989840420A59ADF3F\",\"appId\":\"wxd0b33231fc543b7b\",\"signType\":\"MD5\",\"nonceStr\":\"1516526141486\"}";
+//            setAttr("json", jsonStr);
+            System.out.println(jsonStr);
+            renderAjaxResult("操作成功", 0, jsonStr);
+        } catch (Exception e) {
+            log.error("系统异常:",e);
+            renderAjaxResult("系统异常", Consts.ERROR_CODE_SYSTEM_ERROR, null);
         }
-        final Transaction transaction=TransactionQuery.me().findById(id);
-        if (transaction==null) {
-            renderAjaxResultForError("订单不存在");
-            return;
-        }
-        
-        // 统一下单文档地址：https://pay.weixin.qq.com/wiki/doc/api/jsapi.php?chapter=9_1
-        
-        Map<String, String> params = new HashMap<>();
-        params.put("appid", appid);
-        params.put("mch_id", partner);
-        params.put("body", OptionQuery.me().findValue("wechat_transferDesc"));
-        params.put("out_trade_no", transaction.getOrderNo());
-        params.put("total_fee", Integer.toString(transaction.getTotleFee().multiply(BigDecimal.valueOf(100.00)).intValue()));
-        
-        String ip = IpKit.getRealIp(getRequest());
-        if (StrKit.isBlank(ip)) {
-            ip = "127.0.0.1";
-        }
-        
-        params.put("spbill_create_ip", ip);
-        params.put("trade_type", TradeType.JSAPI.name());
-        params.put("nonce_str", Long.toString(System.currentTimeMillis() / 1000));
-        params.put("notify_url", notifyUrl);
-        params.put("openid", openId);
-        System.out.println("微信统一下单输入参数：" + params);
-        
-        String sign = PaymentKit.createSign(params, paternerKey);
-        params.put("sign", sign);
-        String xmlResult = PaymentApi.pushOrder(params);
-        
-        System.out.println("微信统一下单返回参数：" + xmlResult);
-        Map<String, String> result = PaymentKit.xmlToMap(xmlResult);
-        
-        String returnCode = result.get("return_code");
-        String returnMsg = result.get("return_msg");
-        if (StrKit.isBlank(returnCode) || !SUCCESS.equals(returnCode)) {
-            renderText(returnMsg);
-            return;
-        }
-        String resultCode = result.get("result_code");
-        if (StrKit.isBlank(resultCode) || !SUCCESS.equals(resultCode)) {
-            renderText(returnMsg);
-            return;
-        }
-        // 以下字段在return_code 和result_code都为SUCCESS的时候有返回
-        String prepayId = result.get("prepay_id");
-        
-        Map<String, String> packageParams = new HashMap<>();
-        packageParams.put("appId", appid);
-        packageParams.put("timeStamp", Long.toString(System.currentTimeMillis() / 1000));
-        packageParams.put("nonceStr", Long.toString(System.currentTimeMillis()));
-        packageParams.put("package", "prepay_id=" + prepayId);
-        packageParams.put("signType", "MD5");
-        String packageSign = PaymentKit.createSign(packageParams, paternerKey);
-        packageParams.put("paySign", packageSign);
-        
-        String jsonStr = JsonUtils.toJson(packageParams);
-        setAttr("json", jsonStr);
-        System.out.println(jsonStr);
-        render("weixinpay.html");
     }
     
+    @Before(Tx.class)
     public void pay() {
         try {
             // 支付结果通用通知文档: https://pay.weixin.qq.com/wiki/doc/api/jsapi.php?chapter=9_7
@@ -146,16 +87,16 @@ public class WechatpayController extends BaseFrontController {
                     //更新订单信息
                     Transaction transaction = new Transaction().findFirst("select * from jp_transaction where order_no = ?", orderNo);
                     if (transaction == null) { //订单不存在
-                        log.error("wechat pay notify error：orderNo [" + orderNo + "] is not exists!params = []" + params);
+                        log.error("微信支付通知错误：orderNo [" + orderNo + "] is not exists!params = []" + params);
                         return;
                     }
                     if ("1".equals(transaction.getStatus()) || "2".equals(transaction.getStatus())) {//订单状态不对
-                        log.error("wechat pay notify error：order status[" + transaction.getStatus() + "] is error!params = []" + params);
+                        log.error("微信支付通知错误：order status[" + transaction.getStatus() + "] is error!params = []" + params);
                         return;
                     }
                     BigDecimal totalFee = new BigDecimal(totalFeeStr).divide(new BigDecimal("100"));
                     if (transaction.getTotleFee().compareTo(totalFee) != 0) { //订单金额与支付金额不相等
-                        log.error("wechat pay notify error：orderTotalFee[" + transaction.getTotleFee() + "] <> payTotalFee[" + totalFee + "]!params = []" + params);
+                        log.error("微信支付通知错误：orderTotalFee[" + transaction.getTotleFee() + "] <> payTotalFee[" + totalFee + "]!params = []" + params);
                         return;
                     }
                     transaction.setTradeNo(tradeNo);
@@ -163,7 +104,7 @@ public class WechatpayController extends BaseFrontController {
                     transaction.setPayed(DateUtils.parse1(payed));
                     boolean res = transaction.update();
                     if (!res) { //订单更新失败
-                        log.error("wechat pay notify error：order update failed!params = []" + params);
+                        log.error("微信支付通知错误：order update failed!params = []" + params);
                         return;
                     }
 
@@ -173,12 +114,12 @@ public class WechatpayController extends BaseFrontController {
                     renderText(PaymentKit.toXml(xml));
                     return;
                 } else {
-                    log.error("wechat pay notify error：result_code =" + resultCode);
+                    log.error("微信支付通知错误：result_code =" + resultCode);
                 }
             }
             renderText("");
         } catch (Exception e) {
-            log.error("wechat pay notify error：", e);
+            log.error("微信支付通知错误：", e);
         }
     }
     
