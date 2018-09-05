367bd8cc4fa31f3c0b9bb2dfc2488446ca6107bb
jianb.jiang <jianb.jiang@hnair.com>
2018-09-05 10:17:57 +0800

    可用优惠券查询BUG修改

--- src/main/resources/sql/add_20180901.sql
+++ src/main/resources/sql/add_20180901.sql
@@ -1,6 +1,6 @@
---消息模版
+-- 消息模版
 insert into `jpress_mall`.`jp_option` ( `option_key`, `option_value`) values ( 'wechat_day_statistic_tempMsg_id', 'RqUPqbnUun6VzQdncKSSyU1vA-aXwRRFFURm9EDlgUQ');
 insert into `jpress_mall`.`jp_option` ( `option_key`, `option_value`) values ( 'wechat_week_statistic_tempMsg_id', 'nFHX8dKjBikIR-FBtHt6DMPLYC-UzeVmQOEwPlRK_mc');
 
---提醒超级管理员
+-- 提醒超级管理员
 insert into `jpress_mall`.`jp_option` ( `option_key`, `option_value`) values ( 'managerOpenId', 'o-2OIxGnn9ul-XyN-rg8WrzWsv_0');
